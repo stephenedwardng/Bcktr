8da6f642b0d2114d11f17a2558d00bedf3efa618
Stephen Edward Ng <stephenedwardng@outlook.com>
2017-08-09 14:08:18 +0100

    Updates controller routes

--- controllers/country.js
+++ controllers/country.js
@@ -2,15 +2,11 @@ var express = require('express');
 var app = express();
 var countryRouter = new express.Router();
 
-// countryRouter.get("/", function(req, res){
-//   res.json({data: "Hello from country router"});
-// });
+var countries = require('../client/src/models/countries');
+var Country = require('../client/src/models/country');
 
-var countries = require('../client/src/models/countries')();
-var country = require('../client/src/models/country');
-
-var countryQuery = require('../client/db/countryQuery');
-var query = new countryQuery();
+var CountryQuery = require('../client/db/countryQuery');
+var query = new CountryQuery();
 
 //country by id
 countryRouter.get('/:id', function(req, res){
@@ -19,35 +15,23 @@ countryRouter.get('/:id', function(req, res){
 
 //country index
 countryRouter.get('/', function(req, res) {
-  //res.json(countries);
   query.all(function(results){
     res.json(results);
   })
 });
 
-//country update
-countryRouter.put('/:id', function(req, res) {
-  var country = new country({
-    name: req.body.name
-  });
-  countries[req.params.id] = country;
-  res.json({data: countries});
-});
-
-//add new country
+//add new country to bucket list
 countryRouter.post('/', function(req, res) {
   var country = new country({
-    name: req.body.name
+    name: req.body.name,
+    capital: req.body.capital,
+    latlng: req.body.latlng,
+    population: req.body.population,
+    flag: req.body.flag
   });
   query.add(country, function(results){
     res.json(results);
   });
-});
-
-//delete country
-countryRouter.delete('/:id', function(req, res) {
-  countries.splice(req.params.id, 1);
-  res.json({data: countries});
 });
 
 module.exports = countryRouter;
